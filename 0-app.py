e71f60306dd4c396873971b2d8ba403cd6a5b278
CHEGEBB <chegephil24@gmail.com>
2024-07-30 17:12:26 +0300

    new

--- 0-app.py
+++ 0-app.py
@@ -1,10 +1,17 @@
-# 0-app.py
+#!/usr/bin/env python3
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def index() -> str:
+    """
+    Handles the root route and renders the '0-index.html' template.
+    
+    Returns:
+        str: Rendered HTML template as a string.
+    """
     return render_template('0-index.html')
 
 if __name__ == '__main__':
